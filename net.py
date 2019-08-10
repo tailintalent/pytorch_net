0830e78d76318f5751d588e8ff5b4c5f7b2aae70
tailin <tailinwu2009@gmail.com>
2019-08-10 12:48:05 -0700

    Added Wide-ResNet

--- net.py
+++ net.py
@@ -519,6 +519,17 @@ def load_model_dict_net(model_dict, is_cuda = False):
                            reparam_mode=model_dict["reparam_mode"],
                            is_cuda=is_cuda,
                           )
+    elif net_type == "Wide_ResNet":
+        model = Wide_ResNet(depth=model_dict["depth"],
+                            widen_factor=model_dict["widen_factor"],
+                            input_channels=model_dict["input_channels"],
+                            output_size=model_dict["output_size"],
+                            dropout_rate=model_dict["dropout_rate"],
+                            is_cuda=is_cuda,
+                           )
+        if "state_dict" in model_dict:
+            model.load_state_dict(model_dict["state_dict"])
+        return model
     elif net_type == "ConvNet":
         return ConvNet(input_channels = model_dict["input_channels"],
                        struct_param = model_dict["struct_param"],
@@ -1412,6 +1423,125 @@ class LSTM(RNNCellBase):
         return criterion(y_pred, target)
     
     def prepare_inspection(self, X, y, **kwargs):
+        return {}
+
+
+# ## Wide ResNet:
+
+# In[ ]:
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
+
+def conv_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.xavier_uniform(m.weight, gain=np.sqrt(2))
+        init.constant(m.bias, 0)
+    elif classname.find('BatchNorm') != -1:
+        init.constant(m.weight, 1)
+        init.constant(m.bias, 0)
+
+class wide_basic(nn.Module):
+    def __init__(self, in_planes, planes, dropout_rate=None, stride=1):
+        super(wide_basic, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, padding=1, bias=True)
+        if dropout_rate is not None:
+            self.dropout = nn.Dropout(p=dropout_rate)
+        else:
+            self.dropout = None
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True),
+            )
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        if self.dropout is not None:
+            out = self.dropout(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += self.shortcut(x)
+
+        return out
+
+class Wide_ResNet(nn.Module):
+    """Adapted from https://github.com/meliketoy/wide-resnet.pytorch/blob/master/networks/wide_resnet.py"""
+    def __init__(
+        self,
+        depth,
+        widen_factor,
+        input_channels,
+        output_size,
+        dropout_rate=None,
+        is_cuda=False,
+    ):
+        super(Wide_ResNet, self).__init__()
+        self.in_planes = 16
+        self.depth = depth
+        self.widen_factor = widen_factor
+        self.input_channels = input_channels
+        self.dropout_rate = dropout_rate
+        self.output_size = output_size
+
+        assert ((depth-4)%6 ==0), 'Wide-resnet depth should be 6n+4'
+        n = (depth-4)//6
+        k = widen_factor
+
+        nStages = [16, 16*k, 32*k, 64*k]
+
+        self.conv1 = conv3x3(self.input_channels,nStages[0])
+        self.layer1 = self._wide_layer(wide_basic, nStages[1], n, dropout_rate, stride=1)
+        self.layer2 = self._wide_layer(wide_basic, nStages[2], n, dropout_rate, stride=2)
+        self.layer3 = self._wide_layer(wide_basic, nStages[3], n, dropout_rate, stride=2)
+        self.bn1 = nn.BatchNorm2d(nStages[3], momentum=0.9)
+        self.linear = nn.Linear(nStages[3], output_size)
+        self.is_cuda = is_cuda
+        if is_cuda:
+            self = self.cuda()
+
+    def _wide_layer(self, block, planes, num_blocks, dropout_rate, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, dropout_rate, stride))
+            self.in_planes = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = F.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+
+        return out
+    
+    @property
+    def model_dict(self):
+        model_dict = {"type": "Wide_ResNet"}
+        model_dict["state_dict"] = self.state_dict()
+        model_dict["depth"] = self.depth
+        model_dict["widen_factor"] = self.widen_factor
+        model_dict["input_channels"] = self.input_channels
+        model_dict["output_size"] = self.output_size
+        model_dict["dropout_rate"] = self.dropout_rate
+        return model_dict
+    
+    def get_regularization(self, *args, **kwargs):
+        return to_Variable([0], is_cuda = self.is_cuda)
+    
+    def prepare_inspection(self, *args, **kwargs):
         return {}
 
 
