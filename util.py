7d2a96a5bfc53e7f9a7853d7727ae834bdf71ac0
tailin <tailinwu2009@gmail.com>
2021-04-11 23:22:05 -0700

    Add random.seed for set_seed.

--- util.py
+++ util.py
@@ -3266,6 +3266,7 @@ def set_seed(seed):
         np.random.seed(seed)
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+        random.seed(seed)
 
 
 def reshape_weight_to_matrix(weight, dim=0):
