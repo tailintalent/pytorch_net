57dbdc5d0571cf6ccec6b985bcffaf96470241ce
tailin <tailinwu2009@gmail.com>
2021-05-02 12:53:02 -0700

    Add clip_grad

--- util.py
+++ util.py
@@ -3664,3 +3664,21 @@ class MineDataset(Dataset):
 
     def __repr__(self):
         return self.__class__.__name__ + f"({len(self)})"
+
+
+def clip_grad(optimizer):
+    """Clip gradient."""
+    with torch.no_grad():
+        for group in optimizer.param_groups:
+            for p in group['params']:
+                state = optimizer.state[p]
+
+                if 'step' not in state or state['step'] < 1:
+                    continue
+
+                step = state['step']
+                exp_avg_sq = state['exp_avg_sq']
+                _, beta2 = group['betas']
+
+                bound = 3 * torch.sqrt(exp_avg_sq / (1 - beta2 ** step)) + 0.1
+                p.grad.data.copy_(torch.max(torch.min(p.grad.data, bound), -bound))
