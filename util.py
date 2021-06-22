1c05d4cf398039a36fd9bdbda47250ecdff85548
tailin <tailinwu2009@gmail.com>
2021-06-22 00:58:11 -0700

    Add LINESTYLE_LIST and MARKER_LIST

--- util.py
+++ util.py
@@ -37,6 +37,8 @@ COLOR_LIST = ["b", "r", "g", "y", "c", "m", "skyblue", "indigo", "goldenrod", "s
              "b", "r", "g", "y", "c", "m", "skyblue", "indigo", "goldenrod", "salmon", "pink",
                   "silver", "darkgreen", "lightcoral", "navy", "orchid", "steelblue", "saddlebrown", 
                   "orange", "olive", "tan", "firebrick", "maroon", "darkslategray", "crimson", "dodgerblue", "aquamarine"]
+LINESTYLE_LIST = ["-", "--", ":", "-."]
+MARKER_LIST = ["o", "+", "x", "v", ".", "D"]
 
 
 def plot_matrices(
