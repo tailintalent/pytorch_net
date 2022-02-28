13a7b1f7b0ab78a37e0eac84e2df3012055ab187
tailin <tailinwu2009@gmail.com>
2022-02-28 11:27:59 -0800

    Add diff_recursive

--- util.py
+++ util.py
@@ -5174,3 +5174,34 @@ def extend_dims(tensor, n_dims, loc="right"):
     else:
         raise
     return tensor
+
+
+def diff_recursive(d1, d2, level='root'):
+    """Recursively compare two objects (dictionary, list, tensor, number, etc.)"""
+    from numbers import Number
+    if isinstance(d1, dict) and isinstance(d2, dict):
+        if d1.keys() != d2.keys():
+            s1 = set(d1.keys())
+            s2 = set(d2.keys())
+            print('{:<20} + {} - {}'.format(level, s1-s2, s2-s1))
+            common_keys = s1 & s2
+        else:
+            common_keys = set(d1.keys())
+
+        for k in common_keys:
+            diff_recursive(d1[k], d2[k], level='{}.{}'.format(level, k))
+
+    elif isinstance(d1, list) and isinstance(d2, list):
+        if len(d1) != len(d2):
+            print('{:<20} len1={}; len2={}'.format(level, len(d1), len(d2)))
+        common_len = min(len(d1), len(d2))
+
+        for i in range(common_len):
+            diff_recursive(d1[i], d2[i], level='{}[{}]'.format(level, i))
+
+    else:
+        result = (d1 == d2)
+        if not isinstance(result, Number):
+            result = result.all()
+        if result == False:
+            print('{:<20} {} != {}'.format(level, d1, d2))
